c5723c9ee52282779274265acaae5541c7f34b66
Chris Livingston <clivingston@gmail.com>
2021-09-05 12:35:03 -0700

    implimented the Rest Operator and the Join Method, to eliminate the for loop command

--- assets/FavApp/favorites.js
+++ assets/FavApp/favorites.js
@@ -1,15 +1,51 @@
 //start
 //Writing a favorites application that saves to txt files. Also able to read txt files from the Terminal with nodeJS
 //cars.txt and women.txt are apart of the Command below
-const fs = require ('fs')
-const [, , action, type, item] = process.argv
+// const fs = require ('fs')
+// const [, , action, type, item] = process.argv
 
-const addFavorites = () => {
-  let favorite = ''
-  for (let i = 4; i < process.argv.length; i++) {
-    favorite += `${process.argv[i]} `
-  }
-  
+// const addFavorites = () => {
+//   let favorite = ''
+//   for (let i = 4; i < process.argv.length; i++) {
+//     favorite += `${process.argv[i]} `
+//   }
+// //without the for loop, replace favorite, with item.
+//                                 // v  
+//   fs.appendFile(`${type}.txt`, `${favorite}, `, err => {
+//     if (err) { console.log(err) }
+//   })
+// }
+
+// const viewFavorites = () => {
+//   fs.readFile(`${type}.txt`, 'utf8', (err, favorites) => {
+//     if (err) { console.log(err) }
+//     favorites = favorites.split(', ')
+//     favorites.pop()
+//     favorites.forEach((favorite, i) => {
+//       console.log(`#${i + 1}: ${favorite}`) //an S at the end of ${favorite} will produce an array strung together 4x
+//     })
+//   })
+// }
+
+// switch (action) {
+//   case 'add':
+//     addFavorites()
+//     break
+//   case 'view':
+//     viewFavorites()
+//     break    
+// }
+
+//end
+
+//start 
+//same as above, but with a Rest/Spread Operator, and Join Method. instead of for loop.
+const fs = require('fs')
+const [, , action, type, ...items] = process.argv
+//                        ^is a rest operator
+const addFavorites = () => { 
+  const favorite = items.join(' ')
+//                  ^is the Join Method
   fs.appendFile(`${type}.txt`, `${favorite}, `, err => {
     if (err) { console.log(err) }
   })
@@ -21,7 +57,7 @@ const viewFavorites = () => {
     favorites = favorites.split(', ')
     favorites.pop()
     favorites.forEach((favorite, i) => {
-      console.log(`#${i + 1}: ${favorite}`) //an S at the end of ${favorite} will produce an array strung together 4x
+      console.log(`#${i + 1}: ${favorite}`) 
     })
   })
 }
@@ -32,7 +68,7 @@ switch (action) {
     break
   case 'view':
     viewFavorites()
-    break    
+    break
 }
 
-//end
+// 
